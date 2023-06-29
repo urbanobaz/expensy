b64c4f225707db9d28194947b0f1eb94ad12e261
Urbano <urbanobaz@yahoo.com>
2023-06-29 12:26:18 -0400

    Quick add to restart vercel build

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -31,7 +31,6 @@ export default async function DashboardPage() {
     <div className="container flex-1 space-y-4 p-8 pt-6">
       <div className="flex items-center justify-between space-y-2">
         <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-        <div className="flex items-center space-x-2"></div>
       </div>
       <Tabs defaultValue="income" className="space-y-4">
         <TabsList>
