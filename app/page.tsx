9bf5f51d97eb6b8c3ac771d91db1e6229a9f447f
Urbano <urbanobaz@yahoo.com>
2023-07-01 11:04:32 -0400

    Removing unused files and folders

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,11 @@
-import Link from "next/link"
-
-import { siteConfig } from "@/config/site"
-import { buttonVariants } from "@/components/ui/button"
-
 export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 px-10 py-8">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
+          Sign up to keep track of <br className="hidden sm:inline" />
+          your income and expenses intuitively.
         </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
-        </p>
-      </div>
-      <div className="flex gap-4">
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.signup}
-          className={buttonVariants({ variant: "destructive", size: "lg" })}
-        >
-          Destructive
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.signup}
-          className={buttonVariants({ variant: "ghost" })}
-        >
-          Ghost
-        </Link>
-      </div>
-      <div className="flex gap-4">
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.signup}
-          className={buttonVariants({ variant: "link" })}
-        >
-          Link
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.signup}
-          className={buttonVariants({ variant: "pink" })}
-        >
-          Pink
-        </Link>
       </div>
     </section>
   )
