d9fcb14b5f40d4515800c8db1a0b7568015caf28
Urbano <urbanobaz@yahoo.com>
2023-07-19 21:53:54 -0400

    Add mobile responsiveness fix for add expense/income form

--- app/add-expense/page.tsx
+++ app/add-expense/page.tsx
@@ -3,12 +3,18 @@
 import { cn } from "@/lib/utils"
 import { Card, CardHeader } from "@/components/ui/card"
 
+import styles from "./AddExpense.module.css"
 import AddExpenseForm from "./AddExpenseForm"
 
 export default function DashboardPage() {
   return (
-    <div className="mx-auto mt-4 flex h-full justify-center">
-      <Card className={cn("w-[380px] px-4")}>
+    <div
+      className={cn(
+        styles.cardWrapper,
+        "m-auto mt-4 flex h-full justify-center"
+      )}
+    >
+      <Card className={cn(styles.card, "max-w-[380px] px-4")}>
         <CardHeader>
           <h3 className="mx-auto mb-2 flex justify-center text-lg font-semibold">
             Expense details
--- app/add-income/page.tsx
+++ app/add-income/page.tsx
@@ -1,16 +1,13 @@
-"use client"
-
 import { cn } from "@/lib/utils"
 import { Card, CardHeader } from "@/components/ui/card"
-import { useToast } from "@/components/ui/use-toast"
 
+import styles from "./AddIncome.module.css"
 import AddIncomeForm from "./AddIncomeForm"
 
 export default function DashboardPage() {
-  const { toast } = useToast()
   return (
     <div className="mx-auto mt-4 flex h-full justify-center">
-      <Card className={cn("w-[380px] px-4")}>
+      <Card className={cn(styles.card, "max-w-[380px] px-4")}>
         <CardHeader>
           <h3 className="mx-auto mb-2 flex justify-center text-lg font-semibold">
             Income details
