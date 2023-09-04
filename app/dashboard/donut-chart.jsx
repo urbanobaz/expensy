cd5d58a1ad1013bc4f952435f11c5d2e2a84e339
Urbano <urbanobaz@yahoo.com>
2023-09-04 14:01:27 -0400

    Adding text for donut chart

--- app/dashboard/donut-chart.jsx
+++ app/dashboard/donut-chart.jsx
@@ -1,8 +1,12 @@
 "use client"
 
+import { useTheme } from "next-themes"
 import { Cell, Pie, PieChart, ResponsiveContainer } from "recharts"
 
+import { currencyFormat } from "@/lib/utils"
+
 const Donut = ({ income, loss }) => {
+  const { theme } = useTheme()
   const data = [
     {
       type: "Profit",
@@ -19,6 +23,15 @@ const Donut = ({ income, loss }) => {
     <div className="py-4" style={{ width: "100%", height: 166 }}>
       <ResponsiveContainer>
         <PieChart>
+          <text
+            fill={theme === "light" ? "black" : "white"}
+            x={"50%"}
+            y={"50%"}
+            textAnchor="middle"
+            dominantBaseline="middle"
+          >
+            {currencyFormat(data[0].amount - data[1].amount)}
+          </text>
           <Pie
             dataKey="amount"
             data={data}
@@ -26,12 +39,11 @@ const Donut = ({ income, loss }) => {
             innerRadius={"70%"}
             outerRadius={"100%"}
             blendStroke={false}
-            stroke="black"
+            stroke={theme === "light" ? "white" : "black"}
           >
             {data.map((dataPoint, index) => (
               <Cell key={index} fill={dataPoint.color} />
             ))}
-            <p className="text-white">500</p>
           </Pie>
         </PieChart>
       </ResponsiveContainer>
